8128eefa73412ebf5151ddec8d2a5ae00e85adc6
Giggle <53515356+neoliuhua@users.noreply.github.com>
2025-03-13 01:33:56 +0800

    first shot
    first ui and vite, tauri is ok

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
+
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'build', // 自定义输出目录
+			assets: 'build',
+			fallback: 'index.html' // may differ from host to host
+		  }),
+		  paths: {
+			base: '',
+		  },
+	},
+	preprocess: vitePreprocess(),
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,46 @@ import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST; //adddd
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
   server: {
     port: 3000,
-    host: true
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 3000,
+        }
+      : undefined,
+
+    watch: {
+      // tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**',"**/node_modules/**"],
+    },
   },
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
-    sourcemap: true
-  }
+    outDir: '../build', // Output files to the `dist` folde
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 });
+
+
