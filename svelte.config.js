d95e6650970f0ca7644f588829f5658a61dce4db
Giggle <53515356+neoliuhua@users.noreply.github.com>
2025-03-13 13:57:11 +0800

    tauri bun sveltekit vite ok
    empty with them worked

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 //import adapter from '@sveltejs/adapter-auto';
-//import adapter from '@sveltejs/adapter-node';
+//auto only support some specific enviroments
+// //import adapter from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	
-
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -19,10 +17,13 @@ const config = {
 			assets: 'build',
 			fallback: 'index.html' // may differ from host to host
 		  }),
+		  //自定义输出路径
 		  paths: {
 			base: '',
 		  },
 	},
+		// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 };
 
